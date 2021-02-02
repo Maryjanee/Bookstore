4ec227ddcc4d0c82fa7c79914a9e9844873cec56
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-02-02 07:26:41 -0800

    Add handleChange and handleSubmit  to BookList

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,19 +1,42 @@
 /* eslint-disable import/extensions */
+
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBook } from '../actions';
+import generateId from '../helpers/generateID';
+
+const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
 const BookForm = () => {
-  const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const dispatch = useDispatch();
+  const [book, setBook] = useState({ title: '', category: '' });
+
+  const handleChange = event => {
+    const { id, value } = event.target;
+    setBook(prevBook => ({ ...prevBook, [id]: value }));
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newBook = { ...book, id: generateId() };
+
+    dispatch(createBook(newBook));
+    setBook({ title: '', category: '' });
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="title">
           Book Title
-          <input type="text" id="book" name="book" />
+          <input onChange={handleChange} type="text" id="title" name="title" />
         </label>
       </div>
       <div>
         <label htmlFor="booksCategory">
           Select a category
-          <select name="category" id="category">
+          <select onChange={handleChange} name="category" id="category">
+            <option value="">Please select a category</option>
             {bookCategories.map(category => (
               <option key={category} value={category}>{category}</option>
             ))}
