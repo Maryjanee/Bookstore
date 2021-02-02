a6bd445fe3fd893a06786dcbe19d2a3c2094e1d7
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-02-01 20:57:21 -0800

    update bookreducer to accept createbook and removebook actions

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,11 +1,20 @@
 /* eslint-disable  no-unused-vars */
 
 const initialState = {
-  books: [],
+  books: [
+    
+  ],
 };
 
 function bookReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case CREATE_BOOK:
+      return [...state, action.book];
+    case REMOVE_BOOK:
+      return [...state.filter(book => book.id !== action.book.id)];
+    default:
+      return state;
+  }
 }
 
 export default bookReducer;
