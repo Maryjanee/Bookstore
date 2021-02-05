4788dbfe9957745dabaf92c47d0a90816b50cfb5
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-02-05 08:46:38 +0100

    Add styles for createbook form

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBook } from '../actions';
 import generateId from '../helpers/generateID';
+import '../styles/BookForm.css';
 
 const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
@@ -26,26 +27,20 @@ const BookForm = () => {
 
   return (
     <div>
-      <h4>Add a new Book</h4>
-      <form onSubmit={handleSubmit}>
+      <h4 className="uppercase">Add a new Book</h4>
+      <form onSubmit={handleSubmit} className="d-grid">
         <div>
-          <label htmlFor="title">
-            Book Title
-            <input onChange={handleChange} type="text" id="title" name="title" />
-          </label>
+          <input onChange={handleChange} type="text" id="title" name="title" placeholder="Book Title" />
         </div>
         <div>
-          <label htmlFor="booksCategory">
-            Select a category
-            <select onChange={handleChange} name="category" id="category">
-              <option value="All">All</option>
-              {bookCategories.map(category => (
-                <option key={category} value={category}>{category}</option>
-              ))}
-            </select>
-          </label>
+          <select onChange={handleChange} name="category" id="category">
+            <option value="">Category</option>
+            {bookCategories.map(category => (
+              <option key={category} value={category}>{category}</option>
+            ))}
+          </select>
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" id="createbook" className="uppercase">Add Book</button>
       </form>
     </div>
 
